9c5d5468578fa73e44b144b5aeeaf758c7b82dde
mohammad AbedAlhaq <145481401+malhaq@users.noreply.github.com>
2025-01-02 13:39:18 +0200

    Time: 8 ms (76.69%) | Memory: 24.2 MB (70.75%) - LeetSync

--- 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.cpp
+++ 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.cpp
@@ -6,16 +6,16 @@ public:
         for(char c:chars){
             alphabet[c - 'a']++;
         }
-        for(string word:words){
+        for(const string& word:words){
             if(goodWord(word,alphabet)){
                 count +=word.length();
             }
         }
         return count;
     }
-    bool goodWord(string word,vector<int>alphabet){
+    bool goodWord(const string& word,vector<int>&alphabet){
         vector<int>alphabetCount(26,0);
-        for(char letter:word){
+        for(const char& letter:word){
             alphabetCount[letter-'a']++;
             if(alphabet[letter-'a']<alphabetCount[letter-'a']){
                 return false;
