950ac7121e3df59991fbd978518311dd0b4e07a0
mohammad AbedAlhaq <145481401+malhaq@users.noreply.github.com>
2025-01-02 13:40:19 +0200

    Time: 7 ms (80.41%) | Memory: 24.1 MB (70.75%) - LeetSync

--- 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.cpp
+++ 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.cpp
@@ -15,7 +15,7 @@ public:
     }
     bool goodWord(const string& word,vector<int>&alphabet){
         vector<int>alphabetCount(26,0);
-        for( char letter:word){
+        for(const char& letter:word){
             alphabetCount[letter-'a']++;
             if(alphabet[letter-'a']<alphabetCount[letter-'a']){
                 return false;
