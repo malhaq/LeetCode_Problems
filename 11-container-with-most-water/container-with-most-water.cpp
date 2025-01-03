9ec67fdbadfc52a2d86e5fb83994e75cdd55c0e3
mohammad AbedAlhaq <145481401+malhaq@users.noreply.github.com>
2025-01-03 15:54:06 +0200

    Time: 0 ms (100.00%) | Memory: 62.9 MB (14.13%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -4,7 +4,7 @@ public:
        int areaMax =0;
        int j = height.size()-1;
        int i =0;
-       while(i!=j){
+       while(i<j){
         int h = min(height[i],height[j]);
         int w = j-i;
         int area = w*h;
