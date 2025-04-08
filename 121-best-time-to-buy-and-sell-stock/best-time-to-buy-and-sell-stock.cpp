1e044f28fb9fc2b62f825ae48c5f8dfa25276b01
mohammad AbedAlhaq <145481401+malhaq@users.noreply.github.com>
2025-04-08 22:34:29 +0400

    Time: 0 ms (100.00%) | Memory: 97.4 MB (61.06%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -4,7 +4,7 @@ public:
         int profit =0;
         int price = prices[0];
         int len =prices.size();
-        for(int i =0;i<len;i++){
+        for(int i =1;i<len;i++){
             profit = max(profit,prices[i]-price);
             price = min(price,prices[i]);
         }
