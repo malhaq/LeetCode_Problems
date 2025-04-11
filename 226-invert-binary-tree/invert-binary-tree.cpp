2f8147fd80fd911da44328bc25ff22902b14e669
mohammad AbedAlhaq <145481401+malhaq@users.noreply.github.com>
2025-04-12 02:00:54 +0400

    Time: 0 ms (100.00%) | Memory: 12.4 MB (59.54%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.cpp
+++ 226-invert-binary-tree/invert-binary-tree.cpp
@@ -6,23 +6,40 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
-        if(!root){
-            return nullptr;
+        if(root == nullptr){
+            return root;
         }
-        TreeNode* temp = root->left;
-        root->left = root->right;
-        root->right = temp;
-        invertTree(root->right);
-        invertTree(root->left);
-
-
+        swapNodes(root);
         return root;
+        
+    }
+    void swapNodes(TreeNode* root){
+        if(root==nullptr){
+            return;
+        }
+        TreeNode* Rchild = root-> right;
+        TreeNode* Lchild = root-> left;
+        if(Rchild!=nullptr && Lchild!=nullptr){
+            root->left = Rchild;
+            root->right = Lchild;
+            swapNodes(root->left);
+            swapNodes(root->right);
+        }else if (Rchild==nullptr&&Lchild!=nullptr){
+            root->right = Lchild;
+            root ->left = nullptr;
+            swapNodes(root->right);
+        }else if (Rchild!=nullptr&&Lchild==nullptr){
+            root->left = Rchild;
+            root ->right = nullptr;
+            swapNodes(root->left);
+        }
+        return;
+        
     }
 };
