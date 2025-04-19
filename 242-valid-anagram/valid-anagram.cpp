e05cfbef65edc6a4e34598ade2ba709f9c7b29b4
mohammad AbedAlhaq <145481401+malhaq@users.noreply.github.com>
2025-04-19 10:39:39 +0400

    Time: 0 ms (100.00%) | Memory: 9.6 MB (74.28%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        int count[26]={0};
+        for(char x:s){
+            count[x-'a']++;
+        }
+        for(char x:t){
+            count[x-'a']--;
+        }
+        for(int i :count){
+            if(i!=0){return false;}
+        }
+        return true;
         sort(s.begin(),s.end());
         sort(t.begin(),t.end());
         if(s.length()!=t.length()){return false;}
